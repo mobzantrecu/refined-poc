bad76972eca39489644c3f5722d54160b642bf40
Nicolas <nicolas@Nicolass-MacBook-Air.local>
2022-11-17 09:21:05 -0300

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { Refine } from "@pankod/refine-core";
+import {
+  notificationProvider,
+  Layout,
+  ReadyPage,
+  ErrorComponent,
+} from "@pankod/refine-antd";
+
+import "@pankod/refine-antd/dist/styles.min.css";
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
+
+function App() {
+  return (
+    <Refine
+      notificationProvider={notificationProvider}
+      Layout={Layout}
+      ReadyPage={ReadyPage}
+      catchAll={<ErrorComponent />}
+      routerProvider={routerProvider}
+      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+    />
+  );
+}
+
+export default App;
